b56c9dbc90a9f2b94e828c9ef1c52fb8dfe1a222
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-12-17 18:25:38 -0600

    added models to create tables. Split items and wishlists into seperate tables. Will need to talk abou if we want to hash for security.

---
+++ models/Wishlists.js
@@ -0,0 +1,46 @@
+// linking Model template and datatypes with sequelize
+const { Model, DataTypes } = require('sequelize');
+// linking connection
+const sequelize = require('../config/connection');
+
+// creating wishlist model
+class Wishlists extends Model {}
+
+// creating new Whislist instance
+Wishlists.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        wishlist_name: {
+            type: DataTypes.String,
+            allowNull: false
+        },
+
+        users_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'users',
+              key: 'id'
+            }
+          },
+          
+        items_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'items',
+              key: 'id'
+            }  
+          }
+        },
+        {
+            sequelize,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'wishlist'
+        }
+ );
+
+ module.exports = Wishlists;
---
+++ models/index.js
@@ -0,0 +1,25 @@
+// liniking Users model 
+const Users = require("./Users");
+// linking Wishlists model
+const Wishlists = require("./Wishlists");
+// linking Items model
+const Items = require("./Items");
+
+// Creating Associations. Linking One users to many wishlists
+Users.hasMany(Wishlists, {
+    foreignKey: 'users_id'
+});
+
+Wishlists.belongsTo(Users, {
+    foreignKey: 'users_id',
+});
+
+Wishlists.hasMany(Items, {
+    foreignKey: 'items_id'
+});
+
+Items.belongsTo(Wishlists, {
+    foreignKey: 'items_id'
+});
+
+module.exports = {Users, Wishlists, Items};
