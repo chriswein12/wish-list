09f5f9024a64a0967e37f7d5a1731424dcf1749e
SMKrizan <smkrizan@gmail.com>
2020-12-19 16:10:12 -0600

    hashing seeds issue

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,27 +1,36 @@
 // linking models
 const { Users } = require('../models');
+const bcrypt = require('bcrypt');
+
+const hashSeeds = async (password) => {
+    let seedPw = await bcrypt.hash(password, 10);
+    console.log('seedPw: ', seedPw);
+    console.log('seedPw-type: ', typeof seedPw)
+    return seedPw
+}
 
 // data array
 const usersData = [
     {
         username: 'billy the kid',
         email: 'theKid@gmail.com',
-        password: 'Tough2crack',
+        password: hashSeeds('Tough2crack')
     },
     {
         username: 'bob the man',
         email: 'theMAN@gmail.com',
-        password: 'NOTTough2crack',
+        password: hashSeeds('NOTTough2crack')
     },
     {
         username: 'SallySuperWoman',
         email: 'KickButt@gmail.com',
-        password: 'SuckerPunch2',
+        password: hashSeeds('SuckerPunch2')
     }
 ]
 
 //seeding users
 const seedUsers = () => Users.bulkCreate(usersData);
+console.log('seedUsers: ', seedUsers);
 
 //exporting
 module.exports = seedUsers;
