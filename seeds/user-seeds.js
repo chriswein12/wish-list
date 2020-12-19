d0921dd91da794ca5909ca261e435e03e4c036c3
SMKrizan <smkrizan@gmail.com>
2020-12-19 16:35:52 -0600

    non-existant tables in mysql

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,7 +3,9 @@ const { Users } = require('../models');
 const bcrypt = require('bcrypt');
 
 const hashSeeds = async (password) => {
-    let seedPw = await bcrypt.hash(password, 10);
+    let salt = await bcrypt.genSalt(10);
+    let seedPw = await bcrypt.hash(password, salt);
+
     console.log('seedPw: ', seedPw);
     console.log('seedPw-type: ', typeof seedPw)
     return seedPw
@@ -14,17 +16,20 @@ const usersData = [
     {
         username: 'billy the kid',
         email: 'theKid@gmail.com',
-        password: hashSeeds('Tough2crack')
+        password: 'Tough2crack'
+        // password: hashSeeds('Tough2crack')
     },
     {
         username: 'bob the man',
         email: 'theMAN@gmail.com',
-        password: hashSeeds('NOTTough2crack')
+        password: 'NOTTough2crack'
+        // password: hashSeeds('NOTTough2crack')
     },
     {
         username: 'SallySuperWoman',
         email: 'KickButt@gmail.com',
-        password: hashSeeds('SuckerPunch2')
+        password: 'SuckerPunch2'
+        // password: hashSeeds('SuckerPunch2')
     }
 ]
 
