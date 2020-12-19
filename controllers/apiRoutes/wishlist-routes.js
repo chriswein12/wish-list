6d0154461f79cf9681e8dc31188547f7964e1709
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-12-18 18:24:19 -0600

    completed wishlist api calls.

--- controllers/apiRoutes/wishlist-routes.js
+++ controllers/apiRoutes/wishlist-routes.js
@@ -1,24 +1,79 @@
 // file contains wishlist-routes
 const router = require('express').Router();
-// const { Users, Wishlists, Items } = require('../../models');
+const { Users, Wishlists, Items } = require('../../models');
 
 // returns all wishlists
 router.get('/', (req, res) => {
-    console.log('route returns all wishlists');
-    res.send('wishlists')
-})
+    Wishlists.findAll()
+    .then(dbWishlistData => res.json(dbWishlistData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // returns selected wishlist
 router.get('/:id', (req, res) => {
-    console.log('route returns one wishlist');
-    res.send('wishlist data')
-})
+    Wishlists.findOne({
+        where: {
+            id:req.params.id
+        },
+        include: [
+            {
+                model: Items,
+                attributes:['id', 'item_name', 'price', 'purchase_location']
+            },
+            {
+                model: Users,
+                attributes:['id', 'username']
+            }
+        ]
+    })
+    .then(dbWishlistData =>{ 
+        if(!dbWishlistData){
+            res.status(404).json({message: 'No wishlist found with this id.'});
+            return;
+        }
+        res.json(dbWishlistData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // create new wishlist
 router.post('/', (req, res) => {
-    console.log('route creates new wishlist');
-    res.send('enter wishlist data')
+    Wishlists.create({
+        wishlist_name: req.body.wishlist_name,
+        user_id: req.body.user_id
+    })
+    .then(dbWishlistData => res.json(dbWishlistData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 })
+
+//Deleteing wishlist. 
+router.delete('/:id', (req, res) => {
+    Wishlists.destroy({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then(dbWishlist => {
+        if (!dbWishlist) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbWishlist);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
 
 
 module.exports = router;
